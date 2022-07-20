21fd868c070ce24bd0bc8546e44f3f05199418e9
Michael Mano <michael.mano26@gmail.com>
2022-07-20 20:43:40 +1000

    Add test to check if inputs exist

---
+++ tests/CreditCard.spec.js
@@ -0,0 +1,31 @@
+import { mount } from "@vue/test-utils";
+import { describe, it, expect } from "vitest";
+import CreditCard from "./src/components/CreditCard.vue";
+
+describe('CreditCard.vue', () => {
+  it('should display header text', () => {
+    const msg = 'Credit card validation';
+    const wrapper = mount(CreditCard)
+    expect(wrapper.find('h2').text()).toEqual(msg)
+  });
+
+  it('should have a name input', () => {
+    const wrapper = mount(CreditCard)
+    expect(wrapper.find('#name'));
+  });
+
+  it('should have a credit card input', () => {
+    const wrapper = mount(CreditCard)
+    expect(wrapper.find('#credit-card-number'));
+  });
+
+  it('should have a expiary input', () => {
+    const wrapper = mount(CreditCard)
+    expect(wrapper.find('#expiary'));
+  });
+
+  it('should have a ccv input', () => {
+    const wrapper = mount(CreditCard)
+    expect(wrapper.find('#ccv'));
+  });
+});
