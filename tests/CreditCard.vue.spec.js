41ab0dc535bb58f6ab69fddb64835ba7ac808ac8
Michael Mano <michael.mano26@gmail.com>
2022-07-21 12:40:22 +1000

    Add credit card class test

--- tests/CreditCard.vue.spec.js
+++ tests/CreditCard.vue.spec.js
@@ -1,38 +1,37 @@
 import { mount } from "@vue/test-utils";
 import { describe, it, expect } from "vitest";
-import CreditCard from "./src/components/CreditCard.vue";
+import VueCreditCard from "./src/components/CreditCard.vue";
 
 describe('CreditCard.vue', () => {
 
   it('should display header text', () => {
     const msg = 'Credit card validation';
-    const wrapper = mount(CreditCard)
+    const wrapper = mount(VueCreditCard)
     expect(wrapper.find('h2').text()).toEqual(msg)
   });
 
   it('should have a name input', () => {
-    const wrapper = mount(CreditCard)
-    expect(wrapper.find());
+    const wrapper = mount(VueCreditCard)
+    expect(wrapper.find('#name'));
   });
 
   it('should have a credit card input', () => {
-    const wrapper = mount(CreditCard)
+    const wrapper = mount(VueCreditCard)
     expect(wrapper.find('#credit-card-number'));
   });
 
-  it('should have a expiary input', () => {
-    const wrapper = mount(CreditCard)
-    expect(wrapper.find('#expiary'));
+  it('should have a expiry input', () => {
+    const wrapper = mount(VueCreditCard)
+    expect(wrapper.find('#expiry'));
   });
 
   it('should have a ccv input', () => {
-    const wrapper = mount(CreditCard)
+    const wrapper = mount(VueCreditCard)
     expect(wrapper.find('#ccv'));
   });
 
   it('should have a submit button', () => {
-    const wrapper = mount(CreditCard)
+    const wrapper = mount(VueCreditCard)
     expect(wrapper.find('button[type=submit'));
   });
-
 });
